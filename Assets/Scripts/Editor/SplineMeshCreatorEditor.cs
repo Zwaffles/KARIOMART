710b3b89bb27abf6b4959f5cb772e710134bee8e
Andreas Zweifel <andreaszweifel@hotmail.com>
2023-09-26 16:07:32 +0200

    created SplineMeshCreator script

---
+++ Assets/Scripts/Editor/SplineMeshCreatorEditor.cs
@@ -0,0 +1,52 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Splines;
+using UnityEngine;
+using UnityEngine.Splines;
+
+namespace Editor
+{
+    [CustomEditor(typeof(SplineMeshCreator))]
+    public class SplineMeshCreatorEditor : UnityEditor.Editor
+    {
+        private SplineMeshCreator _splineMeshCreator;
+    
+        private void Awake()
+        {
+            _splineMeshCreator = target as SplineMeshCreator;
+        }
+
+        private void OnEnable()
+        {
+            EditorSplineUtility.AfterSplineWasModified += OnSplineChanged;
+            _splineMeshCreator.OnValueChanged += OnValueChanged;
+        }
+
+        private void OnDisable()
+        {
+            EditorSplineUtility.AfterSplineWasModified -= OnSplineChanged;
+            _splineMeshCreator.OnValueChanged -= OnValueChanged;
+        }
+    
+        private void OnSplineChanged(Spline spline)
+        {
+            if (!_splineMeshCreator.AutoUpdate)
+                return;
+            
+            Debug.Log("Spline form has been changed");
+            
+            _splineMeshCreator.BuildMesh();
+        }
+
+        private void OnValueChanged()
+        {
+            if (!_splineMeshCreator.AutoUpdate)
+                return;
+    
+            Debug.Log("Spline values have been changed");
+            
+            _splineMeshCreator.BuildMesh();
+        }
+    }
+}
+#endif
---
+++ Assets/Scripts/SplineMeshCreator.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class SplineMeshCreator : MonoBehaviour
+{
+    [SerializeField] private SplineContainer splineContainer;
+
+    [SerializeField] private MeshFilter meshFilter;
+
+    [SerializeField] private float width;
+
+    [SerializeField] private int resolution;
+
+    [SerializeField] private bool autoUpdate;
+
+    public bool AutoUpdate => autoUpdate;
+
+    private int _splineIndex;
+
+    private bool _subscribedToEvent;
+    
+    private float3 _forward;
+    private float3 _position;
+    private float3 _upVector;
+
+    private List<Vector3> _vertsP1;
+    private List<Vector3> _vertsP2;
+
+    /// <summary>
+    /// Invoked once the component is loaded or a value has been changed in the inspector
+    /// </summary>
+    public event Action OnValueChanged;
+    
+    private void SampleSplineWidth(float time, out Vector3 p1, out Vector3 p2)
+    {
+        splineContainer.Evaluate(_splineIndex, time, out _position, out _forward, out _upVector);
+
+        float3 right = Vector3.Cross(_forward, _upVector).normalized;
+        p1 = _position + right * width;
+        p2 = _position + -right * width;
+    }
+
+    private void GetVerts()
+    {
+        _vertsP1 = new List<Vector3>();
+        _vertsP2 = new List<Vector3>();
+
+        var step = 1f / resolution;
+        for (var i = 0; i < resolution; i++)
+        {
+            var time = step * i;
+            SampleSplineWidth(time, out var p1, out var p2);
+            _vertsP1.Add(p1);
+            _vertsP2.Add(p2);
+        }
+    }
+
+    public void BuildMesh()
+    {
+        GetVerts();
+
+        var mesh = new Mesh();
+
+        var verts = new List<Vector3>();
+        var tris = new List<int>();
+
+        var length = _vertsP2.Count;
+
+        // Iterates between verts and builds faces
+        for (var i = 1; i <= length; i++)
+        {
+            var p1 = _vertsP1[i - 1];
+            var p2 = _vertsP2[i - 1];
+            Vector3 p3;
+            Vector3 p4;
+
+            if (i == length)
+            {
+                p3 = _vertsP1[0];
+                p4 = _vertsP2[0];
+            }
+            else
+            {
+                p3 = _vertsP1[i];
+                p4 = _vertsP2[i];
+            }
+
+            var offset = 4 * (i - 1);
+
+            var t1 = offset + 0;
+            var t2 = offset + 2;
+            var t3 = offset + 3;
+
+            var t4 = offset + 3;
+            var t5 = offset + 1;
+            var t6 = offset + 0;
+
+            verts.AddRange(new List<Vector3> { p1, p2, p3, p4 });
+            tris.AddRange(new List<int> { t1, t2, t3, t4, t5, t6 });
+
+            mesh.vertices = verts.ToArray();
+            mesh.triangles = tris.ToArray();
+
+            meshFilter.sharedMesh = mesh;
+        }
+    }
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        EditorApplication.delayCall += _OnValidate;
+    }
+
+    private void _OnValidate()
+    {
+        OnValueChanged?.Invoke();
+    }
+}
+#endif
